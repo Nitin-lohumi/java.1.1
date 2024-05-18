d462a3fccb89be1850fd6e3ce7cb012f8e643797
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-05-18 11:55:30 +0530

    add synchronization

--- Thread_Join_Alive.java
+++ Thread_Join_Alive.java
@@ -10,15 +10,13 @@ class Ab implements Runnable{
       for(int i=0; i<3; i++){
         System.out.println("thread"+i);
       }
-    }catch(Exception e){
+    }
+    catch(Exception e){
         System.out.println("Error occur");
     }
   System.out.println("Exit");
-
   }
-
 }
-
 public class Thread_Join_Alive {
 public static void main(String args[]){
   Ab a= new Ab("thread 1");
@@ -28,6 +26,9 @@ public static void main(String args[]){
   a.t.start();
   b.t.start();
   c.t.start();
+  a.t.setPriority(4);
+  System.out.println(b.t.getPriority());
+  System.out.println(a.t.getPriority());
   System.out.println("is alive "+ a.t.isAlive());
   System.out.println("is alive "+ b.t.isAlive());
   System.out.println("is alive "+ c.t.isAlive());
