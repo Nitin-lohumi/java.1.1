80dda5ab5c2707583a4f2168393c02727318fdbe
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-05-24 17:55:15 +0530

    add

---
+++ Serialization.java
@@ -0,0 +1,42 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+class Demo implements java.io.Serializable{
+    public int a ;
+    public String str;
+  public Demo(int a , String str){
+      this.a = a;
+      this.str = str;
+    }
+}
+public class Serialization {
+    public static void main(String[] args) {
+        Demo d = new Demo(1, "nitin");
+        String filename ="Serialization.txt";
+        try {
+        FileOutputStream file  = new FileOutputStream(filename);
+        ObjectOutputStream out  = new ObjectOutputStream(file);
+        out.writeObject(d);
+        out.close();
+        file.close();
+        System.out.println("the object is serailization");
+        } catch (Exception e) {
+            System.out.println("incought cougght exception");
+        }
+        Demo object1 = null;
+        try {
+            FileInputStream file  = new FileInputStream(filename);
+            ObjectInputStream in = new ObjectInputStream(file);
+            object1 = (Demo)in.readObject();
+            in.close();
+            file.close();
+            System.out.println("the object is deserialized");
+            System.out.println("a = " +object1.a);
+            System.out.println("string  = "+ object1.str);
+        } catch (Exception e) {
+           System.out.println("input output exception");
+        }
+    }
+}
