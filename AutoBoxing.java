e824b106dd327ff59842201062bb75c6c54a6eca
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-05-25 12:54:09 +0530

    autoboxing

--- AutoBoxing.java
+++ AutoBoxing.java
@@ -1,8 +1,11 @@
 public class AutoBoxing {
+@SuppressWarnings("removal")
 public static void main(String[] args) {
     int a =100;
     Integer a1 = new Integer(a);
     //AutoBoxing.java:4: warning: [removal] Integer(int) in Integer has been deprecated and marked for removal
     System.out.println(a1);
+    int b = a1;
+    System.out.println("unboxing "+ b);
 }    
 }
